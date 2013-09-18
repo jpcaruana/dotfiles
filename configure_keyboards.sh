fcdf74ae664d0fd6fbda1aae814a35fbd8d9257c
Jean-Philippe CARUANA <jp@target2sell.com>
2013-09-18 18:35:42 +0200

    dvorax detection on my asus laptop

--- configure_keyboards.sh
+++ configure_keyboards.sh
@@ -1,4 +1,4 @@
-DVORAK=`xinput list | grep TypeMatrix | grep keyboard | sed 's/.*id=\([0-9]*\).*/\1/'`
-AZERTY=`xinput list | grep TypeMatrix | grep keyboard | sed 's/.*id=\([0-9]*\).*/\1/'`
+DVORAK=`xinput list | grep TypeMatrix | grep -v pointer | sed 's/.*id=\([0-9]*\).*/\1/'`
+AZERTY=`xinput list | grep XTEST | grep -v pointer | sed 's/.*id=\([0-9]*\).*/\1/'`
 setxkbmap -device $DVORAK dvorak -option compose:ralt
 setxkbmap -device $AZERTY fr -option ""
